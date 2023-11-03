7b7bef9e4d5676337ced69c7955f6f68e7ace6e4
wollex <alexander.wollex.schmidt@googlemail.com>
2023-11-03 14:03:49 +0100

    changed video display default to false

--- parameters.py
+++ parameters.py
@@ -58,7 +58,7 @@ CaImAn = {
     # 'path_to_model': '~/caiman_data/model/cnn_model_online.h5',
 
     ### display during online
-    'show_movie': True,
+    'show_movie': False,
     'save_online_movie': False,
     # 'movie_name_online': "test_mp4v.avi"
 
