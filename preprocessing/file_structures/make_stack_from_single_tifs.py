719345b242c857e82f1d18fb19cf53d978e50e9b
woxxel <alexander.wollex.schmidt@googlemail.com>
2024-06-21 22:42:28 +0200

    adde function to convert h5 to tiff stack (though not sure if working properly)

--- preprocessing/file_structures/make_stack_from_single_tifs.py
+++ preprocessing/file_structures/make_stack_from_single_tifs.py
@@ -3,6 +3,8 @@ import os, tqdm
 import shutil
 import numpy as np
 
+import h5py
+
 def make_stack_from_single_tifs(in_folder,out_folder,file_name=None,T_max=np.inf,data_type=None,normalize=True,clean_after_stacking=False):
 
     """
@@ -100,4 +102,78 @@ def make_stack_from_single_tifs(in_folder,out_folder,file_name=None,T_max=np.inf
             shutil.rmtree(in_folder)
         except:
             pass
+    return out_path
+
+
+
+
+def make_stack_from_h5(in_file,out_folder,T_max=np.inf,data_type=None,normalize=True,clean_after_stacking=False):
+
+    """
+        Creates a stack of tif-files at path 'out_path' from all tif_files found in 'in_folder'.
+
+        TODO:
+            [ ] extend to accepting tiff-files with several layers
+            [ ] extend to accepting a list of in_folders
+
+        input:
+            in_folder (string)
+                path of the folder from which to process tiff-files
+            out_path (string)
+                path where to put the created tiff-stack
+
+        returns:
+            nothing
+    """
+    
+    f = h5py.File(in_file,'r')
+    dataset_key = list(f.keys())[0]
+    data = f[dataset_key]
+
+    basename = os.path.basename(in_file)
+    fname = os.path.splitext(basename)[0] + '.tif'
+    out_path = os.path.join(out_folder,fname)
+
+    dims = data.shape
+    T_total = dims[0]
+    # d = dims[1:]
+
+    data_type_in = data.dtype
+
+    os.makedirs(out_folder,exist_ok=True)
+    
+    T = min(T_max,T_total)
+    
+    data_type = data_type if data_type else data_type_in
+    print(f"Now writing input .h5 file into a tiff stack {out_path} with d={dims} frames ({T_total} found) and datatype {data_type}")
+
+    if normalize:
+        if str(data_type_in).startswith('float'):
+            normalize = False
+        else:
+            max_val = np.iinfo(data_type_in).max
+    print(data_type,max_val)
+
+    memmap_image = memmap(out_path,shape=dims,dtype=data_type)
+
+    # writing data to memmapped tif-file
+    for t in tqdm.tqdm(range(T)):
+        if normalize:
+            memmap_image[t,...] = data[t,...]/max_val
+        else:
+            memmap_image[t,...] = data[t,...]
+
+        # commit changes to file every so often to avoid memory overuse
+        if (not ((t+1)%2000) or (t+1)==T):
+            memmap_image.flush()
+
+
+    del memmap_image
+
+    # if clean_after_stacking:
+    #     print("Removing tmp files...")
+    #     try:
+    #         shutil.rmtree(in_folder)
+    #     except:
+    #         pass
     return out_path
